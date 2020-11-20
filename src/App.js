0c47139ca01f2b142a0455242fe60d6f765a553f
Surahutomo Pradana <surahutomo.pradana@btpn.com>
2020-11-20 22:45:19 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {EVENT_MICROFRONTEND, ADMIN_MICROFRONTEND} from "./config/config";
+import MicroFrontend from "./MicroFrontend";
+
+class App extends Component {
+  render() {
+    const EventMicroFrontend = ({ history }) => (
+      <MicroFrontend history={history} host={EVENT_MICROFRONTEND} name="Event" />
+    );
+
+    const AdminMicroFrontend = ({ history }) => (
+      <MicroFrontend history={history} host={ADMIN_MICROFRONTEND} name="Admin" />
+    );
+
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <div>
+            Container
+          </div>
+          <Switch>
+            <Route exact path="/event" component={EventMicroFrontend} />
+            <Route exact path="/admin" component={AdminMicroFrontend} />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
